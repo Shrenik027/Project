d702de365941a4472d68f9cc1e2d046cedbe0c3a
shrenik9477 <shrenik9477@gmail.com>
2024-06-20 14:15:59 +0530

    updated

---
+++ Web-Dev/Js-Section/5.Arrays/script.js
@@ -0,0 +1,45 @@
+//----
+//!Creating Array using Array Literal
+//----
+
+//sytax
+
+const studentage = [20, 30, 25, 50];
+// console.log(studentage);
+
+//Declaring an array of string
+
+const days = ["monday", "tuesday", "wed", "thu", "fri"];
+// console.log(days);
+
+//Declaring an array of mix types
+
+const mixedtypes = [42, "life", true];
+
+//declaring an array with empty data
+
+const empty = [];
+
+//----
+//!Creating Array using Array Constructor
+//----
+
+//Create array with empty data
+
+const emptydata = new Array();
+// console.log(emptydata);
+
+//Creating an array with predefined size (5)
+
+const sizedef = new Array(5);
+// console.log(sizedef);
+
+//array with element
+
+const fruit = new Array("applr", "banana", "Kiwi");
+// console.log(fruit);
+
+//using mixed types
+
+const mixeddata = new Array(23, "yes", false);
+// console.log(mixeddata);
