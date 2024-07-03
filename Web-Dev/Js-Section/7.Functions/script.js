4d49c177346058819f2fd43043d5ad15b89a3c81
shrenik9477 <shrenik9477@gmail.com>
2024-07-03 21:56:09 +0530

    updated

--- Web-Dev/Js-Section/7.Functions/script.js
+++ Web-Dev/Js-Section/7.Functions/script.js
@@ -39,9 +39,9 @@ const hello = function () {
 
 //hello();
 
-const greetings = function () {
-  console.log("Welcome");
-};
+// const greetings = function () {
+//   console.log("Welcome");
+// };
 
 //invoke
 
@@ -73,7 +73,7 @@ const num = function () {
 //--
 
 (function () {
-  console.log("welcome");
+  // console.log("welcome");
 })();
 
 //calculating Square of a number
@@ -81,5 +81,88 @@ const num = function () {
 (function () {
   const num = 5;
   const square = num * num;
-  console.log(square);
+  //console.log(square);
 })();
+
+//--
+//!--Function Parameters vs Function Arguments
+//--
+
+//DRY-Dont Repeat Yourself
+
+const greetings = function (name, age) {
+  //console.log(name);
+  //console.log(age);
+  console.log(`Welcome ${name} your age is ${age}`);
+};
+
+//greetings("Shrenik", 19);
+
+//--
+//!--Default Parameters
+//--
+
+const greet = function (name = "Guest", age = 19) {
+  console.log(`my name is ${name} and my age is ${age}`);
+};
+//greet("Shrenik", 18);
+
+//Default Parameter with Expression
+
+const multiply = function (a, b = a * 2) {
+  const result = a * b;
+  console.log(`the result is ${result}`);
+};
+
+//multiply(2);
+
+//--
+//!--Return Statement
+//--
+
+const add = function (a, b) {
+  return (results = a + b);
+};
+
+const addResult = add(2, 3);
+
+console.log(addResult);
+
+//!Return Statement with Conditionals
+
+function isEven(num) {
+  if (num % 2 === 0) {
+    return "it is even";
+  } else {
+    return " its odd ";
+  }
+}
+
+//console.log(isEven(25));
+
+//--
+//!--Function Scope
+//--
+
+//Local Variable in Function
+
+function showLocalVar() {
+  const localVar = "I Am A LOCAL VARIABLE";
+  console.log(localVar);
+}
+
+showLocalVar();
+//console.log(localVar); // Reference Error
+
+function Outerfn() {
+  const outervar = "i am outer variable";
+  console.log(outervar);
+  //console.log(innervar);  //ref error
+  function Innerfn() {
+    const innervar = "i am inner Variable";
+    console.log(outervar);
+    console.log(innervar);
+  }
+  Innerfn();
+}
+Outerfn();
