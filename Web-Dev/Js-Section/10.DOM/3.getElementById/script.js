8dab1c4182acc4261a4e8645798b46dbffc8d308
shrenik9477 <shrenik9477@gmail.com>
2024-07-07 10:57:37 +0530

    updated

---
+++ Web-Dev/Js-Section/10.DOM/3.getElementById/script.js
@@ -0,0 +1,24 @@
+//--
+//!--Selecting Elementss using getElementById
+const conferenceTitleEl = document.getElementById("conferenceTitle");
+
+//console.log(conferenceTitleEl.innerHTML);
+
+const conferenceDateEl = document.getElementById("conferenceDate");
+
+const locationTitleEl = document.getElementById("locationTitle");
+
+const conferenceLocationEl = document.getElementById("conferenceLocation");
+
+const speakersTitleEl = document.getElementById("speakersTitle");
+
+const speakersListEl = document.getElementById("speakersList");
+
+console.log({
+  conferenceTitleEl,
+  conferenceDateEl,
+  locationTitleEl,
+  conferenceLocationEl,
+  speakersTitleEl,
+  speakersListEl,
+});
